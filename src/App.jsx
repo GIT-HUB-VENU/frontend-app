bcb35bc66398c116a2e79757c436a39415c43052
GIT-HUB-VENU <venugopal5880844@gmail.com>
2026-03-11 12:09:04 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import Header from './components/Header.jsx'
-import Content from './components/Content.jsx'
-import Footer from './components/Footer.jsx'
-function App() {
+import Header from "./components/Header.jsx";
+import Content from "./components/Content.jsx";
+import Footer from "./components/Footer.jsx";
+import Login from "./components/Login.jsx";
+import Register from "./components/Register.jsx";
+import Cart from "./components/Cart.jsx";
+import Orders from "./components/Orders.jsx";
 
-  return (<div>
-    <Header/>
-    <Content/>
-    <Footer/>
-  </div>
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <Content />
+
+      <Login />
+
+      <Register />
+
+      <Cart />
+
+      <Orders />
+
+      <Footer />
+    </div>
   );
 }
-
-export default App
+export default App;
