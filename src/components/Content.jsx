38f1129bfb71a6d9956d2bd861ed04025f3980fe
GIT-HUB-VENU <venugopal5880844@gmail.com>
2026-03-11 13:18:12 +0530

    updated

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,43 +1,49 @@
 import { useState, useEffect } from "react";
-import "./Content.css";
 import axios from "axios";
+import "./Content.css";
+
 const API_URL = import.meta.env.VITE_API_URL;
+
 function Content() {
-  const [count, setCount] = useState(0);
-  const [products, setProducts] = useState([]);
-  const increment = () => {
-    setCount(count + 1);
-  };
-  const decrement = () => {
-    setCount(count - 1);
-  };
-  const fetchProducts = async () => {
-    const url = `${API_URL}/store`;
-    const res = await axios.get(url);
-    setProducts(res.data);
-  };
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-  return (
-    <div>
-      <h3>Products Page</h3>
-      <button onClick={decrement}>-</button>
-      {count}
-      <button onClick={increment}>+</button>
-      <hr />
-      
-        {products.map((product) => (
-          <div className="products">
-            <ul>
-              <img src={`${API_URL}/${product.image}`} alt={product.name} />
-              <li>Product : {product.name}</li>
-              <li>Price : {product.price}</li>
-              <li>Description : {product.desc}</li>
-            </ul>
-          </div>
-        ))}
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    const [products, setProducts] = useState([]);
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const decrement = () => {
+        setCount(count - 1);
+    };
+
+    const fetchProducts = async () => {
+        const url = `${API_URL}/store`;
+        const res = await axios.get(url);
+        setProducts(res.data);
+    };
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+    return (
+        <div>
+            <h3>Products Page</h3>
+            <button onClick={decrement}>-</button>
+            {count}
+            <button onClick={increment}>+</button>
+            <hr />
+            <div className="row"> <ol>
+                {products.map((product) => (
+                    <div className="products">
+                        <img src={`${API_URL}/${product.image}`} width="300px" alt={product.name} />
+                        <h3>Product Name: {product.name}</h3>
+                        <p>Description: {product.desc}</p>
+                        <h4>Price: ${product.price}</h4>
+                        <p><button>Add to Cart</button></p>
+                    </div>
+                ))}
+            </ol>
+            </div>
+        </div>
+    );
 }
 export default Content;
