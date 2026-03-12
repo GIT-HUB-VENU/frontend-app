dfe29566da9fcd3c5e83e36ce8646c8f8af91496
GIT-HUB-VENU <venugopal5880844@gmail.com>
2026-03-12 11:44:42 +0530

    updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,42 @@
-import { Link } from "react-router-dom";    
-import { use, useState } from "react";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AppContext } from "../App";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 function Login() {
-    const [user, setUser] = useState({});
-    const API_URL = import.meta.env.VITE_API_URL;
-    const Navigate = useNavigate();
-    return (
-        <div className="Login">
-            <h1>Login</h1>
-            <form>
-                <input type="email" placeholder="Email" /><br />
-                <input type="password" placeholder="Password" /><br />
-                <button type="submit">Login</button>
-            </form>
-             <p><Link to="/register">New user register here</Link></p>
-        </div>
-    );
+  const { user, setUser } = useContext(AppContext);
+  const API_URL = import.meta.env.VITE_API_URL;
+  const Navigate = useNavigate()
+  const handleLogin = async () => {
+    const url = API_URL + "/auth/signin";
+    const response = await axios.post(url, user);
+    setUser(response)
+    Navigate("/")
+  };
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <p>
+        <input
+          type="text"
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="Email"
+        />
+      </p>
+      <p>
+        <input
+          type="password"
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          placeholder="Password"
+        />
+      </p>
+      <p>
+        <button onClick={handleLogin}>Login</button>
+      </p>
+      <p>
+        <Link to="/register">New user register here</Link>
+      </p>
+    </div>
+  );
 }
-
 export default Login;
