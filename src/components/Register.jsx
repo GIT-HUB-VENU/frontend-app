46c9e8efb6c541b0bf7bd1e6829a3374c96ed6d1
GIT-HUB-VENU <venugopal5880844@gmail.com>
2026-03-12 15:00:29 +0530

    updated

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -7,52 +7,59 @@ function Register() {
   const [user, setUser] = useState({});
   const API_URL = import.meta.env.VITE_API_URL;
   const Navigate = useNavigate();
+  const handleSubmit = async (e) => {
+  e.preventDefault();   // VERY IMPORTANT
 
-  const handleSubmit = async () => {
-    console.log(user);
+  console.log(user);
 
-    const url = API_URL + "/auth/signup";
+  const url = `${API_URL}/auth/signup`;
 
+  try {
     const response = await axios.post(url, user);
-
+    console.log(response.data);
     Navigate("/login");
-  };
+  } catch (error) {
+    console.log("Error:", error);
+  }
+};
 
   return (
     <div>
-      <h2>Registration Page</h2>
+      <form onSubmit={handleSubmit}>
+  <h2>Registration Page</h2>
 
-      <p>
-        <input
-          type="text"
-          onChange={(e) => setUser({ ...user, name: e.target.value })}
-          placeholder="Name"
-        />
-      </p>
+  <p>
+    <input
+      type="text"
+      onChange={(e) => setUser({ ...user, name: e.target.value })}
+      placeholder="Name"
+    />
+  </p>
 
-      <p>
-        <input
-          type="text"
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-          placeholder="Email"
-        />
-      </p>
+  <p>
+    <input
+      type="text"
+      onChange={(e) => setUser({ ...user, email: e.target.value })}
+      placeholder="Email"
+    />
+  </p>
 
-      <p>
-        <input
-          type="password"
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-          placeholder="Password"
-        />
-      </p>
+  <p>
+    <input
+      type="password"
+      onChange={(e) => setUser({ ...user, password: e.target.value })}
+      placeholder="Password"
+    />
+  </p>
 
-      <p>
-        <button onClick={handleSubmit}>Submit</button>
-      </p>
+  <p>
+    <button type="submit">Submit</button>
+  </p>
 
-      <p>
-        <Link to="/login">Already a member? Login here</Link>
-      </p>
+  <p>
+    <Link to="/login">Already a member? Login here</Link>
+  </p>
+</form>
     </div>
   );
 }
