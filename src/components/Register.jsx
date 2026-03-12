e19b5816dab10b0a2659b5e8482256839177b3dd
GIT-HUB-VENU <venugopal5880844@gmail.com>
2026-03-12 10:20:32 +0530

    updated

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 function Register() {
+    const [user, setUser] = useState({});
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle registration logic here
+    };
+
     return (
         <div>
             <h1>Register Page</h1>
-            <form>
-                <input type="text" placeholder="Username" /><br />
-                <input type="email" placeholder="Email" /><br />
-                <input type="password" placeholder="Password" /><br />
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUser({...user, name: e.target.value})}
+                /><br />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setUser({...user, email: e.target.value})}
+                /><br />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setUser({...user, password: e.target.value})}
+                /><br />
                 <button type="submit">Register</button>
+                <Link to="/login">Already Have Account ? Login</Link>
             </form>
         </div>
     );
